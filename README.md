099be3bc237f2490d16355fd3f1329d370842d6b
SylviaGit4 <torchicky139@gmail.com>
2026-05-12 14:54:35 +0100

    v1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# L-System 2D Renderer
+
+## Demo/Saved Rules
+- Simple "Cyber Tree" : `"F": "FF[-F+F][+F-F]"`
--- main.py
+++ main.py
@@ -3,3 +3,53 @@ import turtle
 def rewrite(axiom, rules, iterations):
     current = axiom
 
+    for i in range(iterations):
+        next_string = "" # resets each iteration
+        for char in current:
+            next_string += rules.get(char, char) # passthrough if no rule
+        current = next_string # swap for next iteration
+    
+    return current
+
+def render_with_branches(lstring, step_size, angle):
+    screen = turtle.Screen()
+    screen.bgcolor("white")
+    screen.tracer(0) # batch render
+
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.left(90) # face upwards
+    stack = [] # empty stack
+
+    for char in lstring:
+        if char == "F":
+            t.forward(step_size)
+        elif char == "+":
+            t.right(angle)
+        elif char == "-":
+            t.left(angle)
+        elif char == "[":
+            state = (t.pos(), t.heading())
+            stack.append(state)
+        elif char == "]":
+            pos, hdg = stack.pop()
+            t.penup()
+            t.setpos(pos)
+            t.setheading(hdg)
+            t.pendown()
+    
+    screen.update()
+    screen.mainloop
+
+    turtle.done()
+
+axiom = "F"
+rules = {
+    "F": "FF[-F+F][+F-F]"
+    }
+iterations = 4
+step = 10
+angle = 60
+
+lstring = rewrite(axiom, rules, iterations)
+render_with_branches(lstring, step, angle)
